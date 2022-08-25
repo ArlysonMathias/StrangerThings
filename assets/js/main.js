3beeb2f81985fd952051f0482d59a782c502ce49
Arlyson Mathias <mathiassouza10@outlook.com>
2022-08-25 15:45:46 -0300

    feature: created form

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,22 @@ window.addEventListener("click", function () {
   audio.volume = 0.2;
 });
 
-window.document.getElementById("txtName")
+// Variáveis
+const txtName = document.getElementById("txtName");
+const txtEmail = document.getElementById("txtEmail");
+const txtLevel = document.getElementById("txtLevel");
+const txtCharacter = document.getElementById("txtCharacter");
+const btnSubscribe = document.getElementById("button-subscribe");
+
+// evento do botão
+
+btnSubscribe.addEventListener("click", () => {
+  const subscription = {
+    name: txtName.value,
+    email: txtEmail.value,
+    level: txtLevel.value,
+    character: txtCharacter.value,
+  };
+
+  console.log(subscription)
+});
